d1435e4ad3b1f7f1873b284a4d488d55bc1bd27f
Will <562165195@qq.com>
2022-01-17 01:15:01 +0800

    增加editText键盘不弹出的设置

---
+++ app/src/main/java/com/binglen/androidxxime/MainActivity.java
@@ -0,0 +1,25 @@
+package com.binglen.androidxxime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        LinearLayout ll_edittext_parents = (LinearLayout) findViewById(R.id.ll_edittext_parents);
+//        ll_edittext_parents.setFocusable(true);
+//        ll_edittext_parents.setDescendantFocusability(ViewGroup.FOCUS_BEFORE_DESCENDANTS);
+//        ll_edittext_parents.requestFocus();
+
+//        EditText edittext = (EditText) findViewById(R.id.edittext);
+//        edittext.clearFocus();
+    }
+}
